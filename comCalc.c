b7700e0a489c673fc513f46c3a7cceae70612069
meaganalfaro <meaganalfaro1@gmail.com>
2024-07-22 16:09:01 -0400

    Create comCalc.c

---
+++ comCalc.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argc, char *argv[]){
+        //Check that you are recieving the correct number of arguments
+        if(argc != 4){
+                printf("Invalid Expression!");
+                exit(0);
+        }
+
+	if(strcmp(argv[2], "+") == 0)
+                printf("Sum is %d\n", atoi(argv[1]) + atoi(argv[3]));
+        else if(strcmp(argv[2], "-") == 0)
+                printf("Difference is %d\n", atoi(argv[1]) - atoi(argv[3]));
+        else if(strcmp(argv[2],"x") == 0)
+                printf("Product is %d\n",atoi(argv[1]) * atoi(argv[3]));
+        else if(strcmp(argv[2], "/") == 0)
+                printf("Division is %d\n",atoi(argv[1]) / atoi(argv[3]));
+        else
+            	printf("Invalid Expression! \n");
+        return 0;
+}
