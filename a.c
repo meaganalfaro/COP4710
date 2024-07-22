bc4edd5bae2f7d8f4d5117c8b56762385280cdf4
meaganalfaro <meaganalfaro1@gmail.com>
2024-07-22 16:09:46 -0400

    Create a.c

---
+++ a.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(int argc, char **argv){//<- can also be *argv[]
+
+        printf("%d \n", argc);
+        printf("%s \n", argv[0]);
+        printf("%s \n", argv[1]);
+
+        return 0;
+}
