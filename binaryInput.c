eec8001604ec81d3acbe9a351ec8dbe530827896
meaganalfaro <meaganalfaro1@gmail.com>
2024-07-22 21:24:54 -0400

    Update binaryInput.c

--- binaryInput.c
+++ binaryInput.c
@@ -6,10 +6,10 @@ int main() {
     printf("Enter file name to write data: ");
     scanf("%s", fileName);
     //ffluh(stdin); <- sometimes does not work!
-    
+    //use this to take the \n char after entering file name
     char c;
     scanf("%c", &c);
-    
+    //pointer to binary file in writing mode
     FILE *write = fopen(fileName, "wb");
     if(write == NULL){
         printf("Unable to open file %s", fileName);
@@ -18,15 +18,17 @@ int main() {
     
     char line[50];
     printf("Enter a line to write in file: ");
+    //fgets line by line and recieves variable, size of variable, and pointer to file
     fgets(line, sizeof(line), stdin);
-    
+    //fwrite pointer to variable, size of line, number of instances, and file pointer 
     fwrite(&line, sizeof(line), 1, write);
     fclose(write);
     
     printf("Data written successfully!\n");
-    
+    // creates a read pointer to binary file in reading mode
     FILE *read = fopen(fileName, "rb");
     char data[50];
+    //fread receives a pointer to variable, size of variable, number of instances, and file pointer
     fread(&data, sizeof(data), 1, read);
     
     printf("Data from the file: %s\n",data);
