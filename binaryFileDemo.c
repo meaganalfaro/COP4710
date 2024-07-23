eb78cde99de8b7e90dafbf70e74614ae9ced5259
meaganalfaro <meaganalfaro1@gmail.com>
2024-07-22 21:19:55 -0400

    Update binaryFileDemo.c

--- binaryFileDemo.c
+++ binaryFileDemo.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 int main() {
+    // creates a pointer to binary file in writing mode
     FILE *write = fopen("myfile.bin", "wb");
     if(write == NULL){
         printf("Unable to create file.\n");
@@ -11,7 +12,7 @@ int main() {
     int num; 
     printf("Enter a number: ");
     scanf("%d", &num);
-    
+    //fwrite recieves pointer to variable, size of variable, number of instance, and file pointer
     fwrite(&num, sizeof(num), 1, write);
     fclose(write);
     
